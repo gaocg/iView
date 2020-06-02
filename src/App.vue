be7f1a7cb2883615d2c5bb042883a41168eade86
unknown <1009841773@qq.com>
2020-06-02 23:11:12 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view  v-if="!$route.meta.keepAlive"></router-view>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
